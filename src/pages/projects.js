7af501ad6e3feda8b7f7308f362ed3eed623dcd6
Hank Mason <hankthemason@gmail.com>
2025-05-14 16:45:43 -0400

    update about and projects

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,18 +1,14 @@
-import React from 'react'
-import Project from '../components/project'
-import {
-  projects,
-  headline
-} from './projects.module.css'
-import Layout from '../components/layout'
-import { graphql } from 'gatsby'
-import { GatsbyImage, getImage } from 'gatsby-plugin-image'
+import React from 'react';
+import Project from '../components/project';
+import { projects, headline } from './projects.module.css';
+import Layout from '../components/layout';
+import { graphql } from 'gatsby';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 
 const Projects = ({ data }) => {
-
-  let images = []
+  let images = [];
   for (const [key] of Object.entries(data)) {
-    images.push(getImage(data[key]))
+    images.push(getImage(data[key]));
   }
 
   return (
@@ -21,7 +17,7 @@ const Projects = ({ data }) => {
         <p>My Projects</p>
       </div>
       <div className={projects}>
-        <Project 
+        <Project
           title='Know Your Cops'
           url='www.knowyourcops.com'
           urlFull='https://knowyourcops.com'
@@ -30,16 +26,7 @@ const Projects = ({ data }) => {
         >
           <GatsbyImage image={images[0]} />
         </Project>
-        <Project 
-          title='Personal Budgeting App'
-          url='www.dollarsdollarsdollars.club'
-          urlFull='https://dollarsdollarsdollars.club'
-          repoUrl='https://github.com/hankthemason/finances-tracker'
-          description='A full-stack web app that allows a user to create a profile and customize the way that they track income and expenses.  It uses Typescript, React, Node.js, and PostgreSQL. (this project is still under active development)'
-        >
-          <GatsbyImage image={images[1]} />
-        </Project>
-        <Project 
+        <Project
           title='Thomas Pynchon Name Generator'
           url='www.pynchoname.com'
           urlFull='https://pynchoname.com'
@@ -47,6 +34,15 @@ const Projects = ({ data }) => {
           description='A TypeScript/React app that generates a name and character description in the style of author Thomas Pynchon based on a user’s input.'
         >
           <GatsbyImage image={images[2]} />
+        </Project>
+        <Project
+          title='Personal Budgeting App'
+          url='www.dollarsdollarsdollars.club'
+          urlFull='https://dollarsdollarsdollars.club'
+          repoUrl='https://github.com/hankthemason/finances-tracker'
+          description='A full-stack web app that allows a user to create a profile and customize the way that they track income and expenses.  It uses Typescript, React, Node.js, and PostgreSQL. (this project is still under active development)'
+        >
+          <GatsbyImage image={images[1]} />
         </Project>
         <Project
           title='Blogr Landing Page Mockup'
@@ -59,20 +55,17 @@ const Projects = ({ data }) => {
         </Project>
       </div>
     </Layout>
-  )
-}
+  );
+};
 
-
-export default Projects
+export default Projects;
 
 export const fluidImage = graphql`
-fragment fluidImage on File {
-  childImageSharp {
-    gatsbyImageData(
-      width: 200
-    )
+  fragment fluidImage on File {
+    childImageSharp {
+      gatsbyImageData(width: 200)
+    }
   }
-}
 `;
 
 export const pageQuery = graphql`
@@ -90,4 +83,4 @@ export const pageQuery = graphql`
       ...fluidImage
     }
   }
-`
+`;
